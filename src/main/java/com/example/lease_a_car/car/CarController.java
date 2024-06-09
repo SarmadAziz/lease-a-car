353d85ae189f81606025295f4bbf9226e2dd4401
sarmadaziz <sarmad.aziz@hotmail.nl>
2024-06-09 14:13:49 +0200

    read in cars csv + populate database + get all cars endpoint

---
+++ src/main/java/com/example/lease_a_car/car/CarController.java
@@ -0,0 +1,25 @@
+package com.example.lease_a_car.car;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/cars")
+public class CarController {
+
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Car>> getCars() {
+        List<Car> cars = carService.getAllCars();
+        return ResponseEntity.ok(cars);
+    }
+}
---
+++ src/main/java/com/example/lease_a_car/car/CarService.java
@@ -0,0 +1,19 @@
+package com.example.lease_a_car.car;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarService {
+
+    private final CarRepository carRepository;
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public List<Car> getAllCars() {
+        return carRepository.findAll();
+    }
+}
